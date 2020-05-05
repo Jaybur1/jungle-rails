386367df1c5acfc1efc6b5167f0a8f5445c9d6da
Jaybur1 <jayburbyga@gmail.com>
2020-05-05 14:49:56 -0400

    testing add_to_cart feature

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-
-
+RSpec.feature "AddToCarts", type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
+
     10.times do |n|
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
@@ -17,15 +16,14 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
+  scenario "They add a product to the cart" do
     # ACT
     visit root_path
-
+    first('.product').click_on('Add')
 
     # DEBUG / VERIFY
+    expect(page).to have_content 'My Cart (1)'
     save_screenshot
-    #VERIFY
-    expect(page).to have_css 'article.product', count: 10
+    
   end
-
 end
